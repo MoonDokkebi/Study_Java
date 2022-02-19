e49b2a25c5ffaa9c290200ee0b9b82e266d12bd6
MoonDokkebi <jsangmin99@gmail.com>
2022-02-19 18:10:06 +0900

    2022-02-19

---
+++ src/Quiz/Quiz01.java
@@ -0,0 +1,43 @@
+//별찍기 문제
+package Quiz;
+import java.util.Scanner;
+
+public class Quiz01 {
+    public static void main(String[] args) {
+        int line,choice;
+        String wordlengh;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("유형을 선택하시오 1.숫자, 2.문자 ");
+        choice = sc.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("줄 수를 입력하시요");
+                line = sc.nextInt();
+                NumStar(line);
+                break;
+            case 2:
+                System.out.println("줄수만큼 문자를 입력하세요");
+                wordlengh = sc.next();
+                WordStar(wordlengh);
+        }
+    }
+
+    static void NumStar(int line){
+        for(int i = 0; i < line; i++){
+            for(int j = 0; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void WordStar(String wordlengh){
+        for(int i = 0; i < wordlengh.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
